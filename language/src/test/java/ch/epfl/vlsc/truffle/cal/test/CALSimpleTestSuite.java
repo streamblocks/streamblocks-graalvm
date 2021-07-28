4ff47e4ba9cec9897f69a46ca174140cb6b8cce8
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-07-28 20:49:23 +0530

    Add testcase from CLR document for testing correct lazy evaluation

--- language/src/test/java/ch/epfl/vlsc/truffle/cal/test/CALSimpleTestSuite.java
+++ language/src/test/java/ch/epfl/vlsc/truffle/cal/test/CALSimpleTestSuite.java
@@ -172,6 +172,11 @@ public class CALSimpleTestSuite extends CALTestSuite {
     }
 
     @Test
+    public void lazyEvaluationEdgecaseTest() throws IOException {
+        runTest(TestCase.newBuilder("lazy-evaluation-edgecase").setActorName("test.test").build());
+    }
+
+    @Test
     public void idctTest() throws IOException {
         runTest(TestCase.newBuilder("dct/src/TopIDCT").setActorName("RVC.TopIDCT").setDirLookup(true).build());
     }
